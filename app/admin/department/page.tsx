ac85f22d63b84e7bb87084b86e5537398471112f
Nidhi <p.nidhi2830@gmail.com>
2024-08-14 13:55:18 -0500

    added department page

---
+++ app/admin/department/page.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@/app/ui/button";
+import Link from "next/link";
+import { DeleteButton, UpdateButton } from "@/app/ui/buttons";
+import { fetchAllDepartments } from "@/lib/data";
+import { notFound } from "next/navigation";
+
+const Department = async () => {
+  const departments = await fetchAllDepartments();
+
+  if (!departments) {
+    notFound();
+  }
+
+  return (
+    <div className="px-5 mt-3">
+      <div className="d-flex justify-content-center">
+        <h3>Department List</h3>
+      </div>
+      <Button variant="secondary" className="border ml-96">
+        <Link href="/admin/department/create">Create Department</Link>
+      </Button>
+      <div className="mt-3">
+        <div className="relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border ">
+          <table className="w-full text-sm text-gray-500">
+            <thead className="text-sm text-gray-700 text-left uppercase bg-gray-50 ">
+              <tr>
+                <th scope="col" className="hidden sm:block px-6 py-3">
+                  ID
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {departments.map((d, index) => (
+                <tr
+                  key={d.id}
+                  className="bg-white border-b group/item hover:bg-black-100"
+                >
+                  <th
+                    scope="row"
+                    className="items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    {index + 1}
+                  </th>
+                  <td className=" py-4">{d.name}</td>
+                  <td className="px-6 py-4">
+                    {/* <Link
+                      className="group/edit invisible hover:color-black-700 group-hover/item:visible ..."
+                      href={/admin/${id}}
+                    ></Link> */}
+                    <UpdateButton url={department/${d.id}} />
+                    <DeleteButton id={d.id} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Department;
